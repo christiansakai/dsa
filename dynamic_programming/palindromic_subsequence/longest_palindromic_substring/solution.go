46c091eff72a020f9ed3d8759a24211b0dfc80cf
Christian Sakai <christianmsakai@gmail.com>
2020-02-05 17:03:53 -0500

    WIP

--- dynamic_programming/palindromic_subsequence/longest_palindromic_substring/solution.go
+++ dynamic_programming/palindromic_subsequence/longest_palindromic_substring/solution.go
@@ -1,6 +1,7 @@
 package solution
 
 import "math"
+import "fmt"
 
 func TopDown(str string) int {
 	if len(str) == 0 {
@@ -9,18 +10,65 @@ func TopDown(str string) int {
 
 	byteStr := []byte(str)
 
-	return recurse(byteStr, 0, len(byteStr)-1)
+  return recurse2(byteStr, 0, 0, "")
 }
 
-func recurse(str []byte, start, end int) int {
-	if start == end {
-		return 1
-	}
+func recurse2(str []byte, i, j int, acc string) int {
+  if i == len(str) {
+    return 0
+  }
 
-	var max float64 = 0
+  if j == len(str) {
+    fmt.Println(str)
+    var max float64 = 0
+    if isPalindrome(acc) {
+      max = float64(len(acc))
+    }
 
-	if str[start] == str[end] {
-		withFrontBack := 2 + recurse(str, start+1, end-1)
-	}
+    nextI := i + 1
+    nextJ := nextI + 1
 
+    iPlus := recurse2(str, nextI, nextJ, "") 
+    max = math.Max(max, float64(iPlus))
+
+    return int(max)
+  }
+
+  jPlus := recurse2(str, i, j + 1, acc + string([]byte{str[j]}))
+  // iPlus := recurse2(str, i + 1, j, acc)
+
+  // result := math.Max(float64(jPlus), float64(iPlus))
+
+  return int(jPlus)
+}
+
+// func recurse(str []byte, index int, acc string) int {
+//   if index == len(str) {
+//     fmt.Println(acc)
+//     if isPalindrome(acc) {
+//       return len(acc)
+//     }
+
+//     return 0
+//   }
+
+//   without := recurse(str, index + 1, acc)
+//   with := recurse(str, index + 1, acc + string([]byte{str[index]}))
+
+//   result := math.Max(float64(with), float64(without))
+
+//   return int(result)
+// }
+
+func isPalindrome(str string) bool {
+  b := []byte(str)
+  length := len(b) / 2
+
+  for i := 0; i < length; i++ {
+    if b[i] != b[len(b) - 1 - i] {
+      return false
+    }
+  }
+
+  return true
 }
--- dynamic_programming/palindromic_subsequence/longest_palindromic_substring/solution_test.go
+++ dynamic_programming/palindromic_subsequence/longest_palindromic_substring/solution_test.go
@@ -8,13 +8,13 @@ func TestSolution(t *testing.T) {
 		Output int
 	}{
 		{"abdbca", 3},
-		{"cddpd", 3},
-		{"pqr", 1},
+		// {"cddpd", 3},
+		// {"pqr", 1},
 	}
 
 	t.Run("Top-down Dynamic Programming with Memoization", func(t *testing.T) {
 		for _, tt := range tests {
-			got := SolveTopDown(tt.Input)
+			got := TopDown(tt.Input)
 			if got != tt.Output {
 				t.Errorf("got %d, want %d", got, tt.Output)
 			}
