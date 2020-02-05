4350b473a7e8129222fcda591fcc970450b4068a
Christian Sakai <christianmsakai@gmail.com>
2020-02-05 11:00:26 -0500

    Unbounded knapsack

--- dynamic_programming/unbounded_knapsack/unbounded_knapsack/solution.go
+++ dynamic_programming/unbounded_knapsack/unbounded_knapsack/solution.go
@@ -9,35 +9,43 @@ type Fruit struct {
 }
 
 func TopDown(fruits []Fruit, capacity int) int {
-	// if len(fruits) == 0 {
-	//   return 0
-	// }
+	if len(fruits) == 0 || capacity == 0 {
+	  return 0
+	}
 
-	// cache := map[int]int{}
+	cache := map[int]map[int]int{}
 
-	// return recurse(fruits, capacity, index, cache)
+	return recurse(fruits, capacity, len(fruits) - 1, cache)
 }
 
-func recurse(fruits []Fruit, capacity, index int, cache map[int]int) int {
-	// if capacity == 0 {
-	//   return 0
-	// }
+func recurse(fruits []Fruit, capacity, index int, cache map[int]map[int]int) int {
+	if capacity == 0 || index == -1 {
+	  return 0
+	}
 
-	// if result, ok := cache[capacity]; ok {
-	//   return result
-	// }
+  if _, ok := cache[capacity]; ok {
+    if result, ok := cache[capacity][index]; ok {
+      return result
+    }
+  }
 
-	// var max float64 = 0
-	// for _, f := range fruits {
-	//   if f.Weight <= capacity {
-	//     selection := f.Profit + recurse(fruits, capacity - f.Weight, cache)
-	//     max = math.Max(max, float64(selection))
-	//   }
-	// }
+  var max float64
 
-	// result := int(max)
+  if fruits[index].Weight <= capacity {
+    with := fruits[index].Profit + recurse(fruits, capacity - fruits[index].Weight, index, cache)
+    max = math.Max(max, float64(with))
+  }
 
-	// cache[capacity] = result
+  without := recurse(fruits, capacity, index - 1, cache)
+  max = math.Max(max, float64(without))
 
-	// return result
+  result := int(max)
+
+  if _, ok := cache[capacity]; !ok {
+    cache[capacity] = map[int]int{}
+  }
+
+  cache[capacity][index] = result
+
+  return result
 }
