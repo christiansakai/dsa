c835d301b17cab563b2545d527dadd13bfe31f5b
Christian Sakai <christianmsakai@gmail.com>
2020-02-11 13:26:53 -0500

    Add topological sort

--- recursion/with_data_structures/topological_sort_graph/solution.go
+++ recursion/with_data_structures/topological_sort_graph/solution.go
@@ -3,24 +3,61 @@ package solution
 import "dsa/data_structures/graph"
 
 func Solve(g *graph.Graph) []int {
-	collect := []int{}
+	nodes := collectAllNodes(g)
+	result := []int{}
 	visited := map[int]bool{}
+	recurse(g, nodes, visited, &result)
 
-	recurse(g, 0, visited, &collect)
-
-	return collect
+	return result
 }
 
-func recurse(g *graph.Graph, node int, visited map[int]bool, collect *[]int) {
-	if visited[node] {
+func collectAllNodes(g *graph.Graph) map[int]bool {
+	nodes := map[int]bool{}
+	for node, neighbors := range g.Dict {
+		nodeInt := node.(int)
+		nodes[nodeInt] = true
+
+		for _, neighbor := range neighbors {
+			neighborInt := neighbor.(int)
+			nodes[neighborInt] = true
+		}
+	}
+
+	return nodes
+}
+
+func recurse(g *graph.Graph, nodes map[int]bool, visited map[int]bool, result *[]int) {
+	node := findUnvisitedNodeWithZeroInDegree(g, visited, nodes)
+	if node == -1 {
 		return
 	}
 
-	*collect = append(*collect, node)
+	*result = append(*result, node)
 	visited[node] = true
+	removeNodeFromOtherInDegrees(g, node)
 
-	for _, neighbor := range g.Dict[node] {
-		neighborInt, _ := neighbor.(int)
-		recurse(g, neighborInt, visited, collect)
+	recurse(g, nodes, visited, result)
+}
+
+func findUnvisitedNodeWithZeroInDegree(g *graph.Graph, visited map[int]bool, nodes map[int]bool) int {
+	inDegrees := map[int]int{}
+
+	for _, neighbors := range g.Dict {
+		for _, neighbor := range neighbors {
+			neighborInt := neighbor.(int)
+			inDegrees[neighborInt] = 1
+		}
 	}
+
+	for node, _ := range nodes {
+		if inDegrees[node] == 0 && !visited[node] {
+			return node
+		}
+	}
+
+	return -1
+}
+
+func removeNodeFromOtherInDegrees(g *graph.Graph, node int) {
+	delete(g.Dict, node)
 }
