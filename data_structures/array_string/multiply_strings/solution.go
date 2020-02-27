8d95e2195da06c6413bfedbfd7c3550fdf938c96
Christian Sakai <christianmsakai@gmail.com>
2020-02-27 12:45:32 -0500

    WIP

--- data_structures/array_string/multiply_strings/solution.go
+++ data_structures/array_string/multiply_strings/solution.go
@@ -1,46 +1,39 @@
 package solution
 
 func Solve(num1, num2 string) string {
-	if num1 == "0" || num2 == "0" {
-		return "0"
-	}
+  result := []int{}
+  carry := 0
 
-	result := make([]byte, len(num1)+len(num2))
-	for i := 0; i < len(result); i++ {
-		res[i] = '0'
-	}
+  for i := len(num1) - 1; i >= 0; i-- {
+    for j := len(num2) - 1; j >= 0; j-- {
+      num1Digit := num1[i]
+      num2Digit := num2[j]
 
-	for i := len(num2) - 1; i >= 0; i-- {
-	}
+      product := (byteToInt(num1Digit) * byteToInt(num2Digit)) + carry
+      productIndex := i + j
 
+      if !(productIndex < len(result)) {
+      }
+    }
+  }
 }
 
-func multiply(num1 string, num2 string) string {
-	if num1 == "0" || num2 == "0" {
-		return "0"
-	}
-	res := []rune(num1 + num2)
-	for i := 0; i < len(num1)+len(num2); i++ {
-		res[i] = '0'
-	} // the above code prepares res as a string of "000...0" of length len(num1)+ len(num2).
-
-	for i := len(num2) - 1; i >= 0; i-- {
-		carry := 0
-		j := len(num1) - 1
-		m := 0
-		for carry != 0 || j >= 0 {
-			m = carry + int(res[i+j+1]-'0')
-			if j >= 0 {
-				m += int(num2[i]-'0') * int(num1[j]-'0')
-			}
-			res[i+j+1] = rune(m%10 + '0')
-			carry = m / 10
-			j--
-		}
-	}
-	count := 0 // to count how many zeros are to be removed in the front of "res"
-	for i := 0; i < len(res) && res[i] == '0'; i++ {
-		count++
-	}
-	return string(res[count:])
-}
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        result, carry = [], 0
+        for num1Idx, num1Digit in enumerate(num1[::-1]):
+            for num2Idx, num2Digit in enumerate(num2[::-1]):
+                digitProduct = (ord(num1Digit) - ord('0')) * (ord(num2Digit) - ord('0')) + carry
+                resultIdx = num1Idx + num2Idx
+                if not resultIdx < len(result):
+                    result.append(0)
+                # imagine if you were summing the results
+                # as you did the multiplication...
+                result[resultIdx] += digitProduct
+                carry = result[resultIdx] // 10
+                result[resultIdx] %= 10
+            if carry:
+                result.append(carry)
+                carry = 0
+        resultStr = "".join(map(str,result[::-1])).lstrip("0")
+        return resultStr if resultStr else "0"
